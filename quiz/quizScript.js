fc2420aa055e3764678fa9d2654a65f1bd2b4048
igorlakic2 <igor.lakic2@gmail.com>
2020-06-21 23:15:28 +0200

    quiz

--- quiz/quizScript.js
+++ quiz/quizScript.js
@@ -23,10 +23,12 @@ function Question(question, answers, correctAnswer) {
     this.correctOrWrong = () => {                
         let a = document.querySelectorAll(".answer");
 
-        for(let i=0; i<this.answers.length; i++){
-            a[i].classList.remove("selected");
-            a[i].addEventListener('click', () => {
-                a[i].classList.add("selected");
+        for(let i=0; i<this.answers.length; i++){    
+            if(a[i].className == 'selected'){ 
+                a[i].classList.toggle("selected");   
+            }   
+            a[i].addEventListener('click', () => {    
+                a[i].classList.add("selected"); 
                 if(this.answers[i] == this.answers[this.correctAnswer]){
                     console.log("TRUE");
                     score += 5;
@@ -36,7 +38,7 @@ function Question(question, answers, correctAnswer) {
                     console.log("False! Try again...");
                     score -= 2;
                     console.log(score);                    
-                }
+                } 
             });
         }
     }
@@ -46,27 +48,23 @@ let q1 = new Question("This is javascript?", ["Yes", "No"], 0);
 let q2 = new Question("5 * 7?", ["5", "61", "20", "35", "40"], 3);
 let q3 = new Question("100 - 22?", ["50", "100", "80", "40", "78"], 4);
 let q4 = new Question("9 * 0?", ["90", "1", "0", "9"], 2);
-let q5 = new Question("1 + 978?", ["5", "1", "2", "3"], 2);
-let q6 = new Question("5 * 45645?", ["5", "61", "20", "35", "40"], 3);
+let q5 = new Question("11 + 20?", ["30", "1120", "21", "31"], 3);
+let q6 = new Question("0 / 45645?", ["0", "0.45645", "infinty"], 0);
 let q7 = new Question("100 - 321?", ["50", "100", "80", "40", "78"], 4);
-let q8 = new Question("9 * 123?", ["90", "1", "0", "9"], 2);
+let q8 = new Question("9 * 10?", ["10", "90", "130", "900"], 1);
 
 let questions = [q1, q2, q3, q4, q5, q6, q7, q8];
-// shuffle(questions);
+shuffle(questions);
 let len = questions.length;
 let x = 0;
 
 let rnd = Math.round(Math.random() * 8);
 
-// questions[x].display();
-// questions[x].correctOrWrong();
-
 finish.addEventListener('click', () => {
-    // alert(`${score}, ${trueAnswer}/${questions.length}`);
     let perc = (trueAnswer / questions.length) * 100;
     displayDiv.innerHTML = `<h1>Your result: ${trueAnswer}/${questions.length}</h1>
                             <h3>${perc}%</h3>
-                            <button>TRY AGAIN</button>
+                            <a href=""><button>TRY AGAIN</button></a>
                             `
 
 })
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const calculator = document.getElementById("calcDiv");
 const clock = document.getElementById("clockDiv");
+const quiz = document.getElementById("quizDiv");
 
 window.addEventListener('load', () => {
     calculator.style.marginLeft = '0';
     calculator.style.opacity = '1';
     clock.style.marginLeft = '0';
     clock.style.opacity = '1';
+    quiz.style.marginLeft = '0';
+    quiz.style.opacity = '1';
 })
